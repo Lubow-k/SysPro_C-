20b28cd75ad94587850b4102d3747e9eaf3e7368
Luba Kolbasova <luba1607@gmail.com>
2024-05-19 18:36:42 +0700

    fix 10 task

--- 10_task/mix_in.hpp
+++ 10_task/mix_in.hpp
@@ -5,13 +5,19 @@ template <typename T, int limit>
 struct Counter {
     inline static int objects_created = 0;
 
-    Counter() {
+    instanceChecker() {
         if (objects_created >= limit) {
             throw std::runtime_error("Instance limit exceeded");
         }
+    }
+
+    Counter() {
+        instanceChecker();
         ++objects_created;
     }
+
     Counter(const Counter&) {
+        instanceChecker();
         ++objects_created;
     }
   protected:
--- 10_task/test_mix_in.cpp
+++ 10_task/test_mix_in.cpp
@@ -30,8 +30,22 @@ void test_2() {
     std::cout << "Success" << std::endl;
 }
 
+void test_3() {
+    try {
+        Foo ob_1;
+        Foo ob_2;
+        Foo ob_0;
+        Foo ob_3(ob_2);
+    } catch(const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+        return;
+    }
+    std::cout << "Success" << std::endl;
+}
+
 int main() {
     test_1();
     test_2();
+    test_3();
     return 0;
 }
