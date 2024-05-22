89e2d5dcedd273bb3c8868d79e4addb1b01f4fa8
Luba Kolbasova <luba1607@gmail.com>
2024-05-22 15:45:36 +0700

    add test for 14 nstt

--- 14_task/allocate.cpp
+++ 14_task/allocate.cpp
@@ -9,9 +9,7 @@ private:
 
 public:
     Pair(int f, int s) : first(f), second(s) {}
-
     Pair(const Pair& other) : first(other.first), second(other.second) {}
-
     int getFirst() const { return first; }
     int getSecond() const { return second; }
 };
@@ -23,24 +21,36 @@ void allocate(void* memory, Types... args) {
   constexpr size_t total_size = (sizeof(Types) + ...) ;
   static_assert(SIZE >= total_size);
   static_assert((std::is_copy_constructible<Types>::value && ...));
+
   auto buffer = static_cast<char*>(memory);
-  bool ok = (((new (buffer) Types(args)) ? (buffer += sizeof(Types), true) : (false)) && ...);
+  auto alloc = [&buffer]<typename T>(T arg) { 
+    new(buffer) T(arg); 
+    buffer += sizeof(T);  
+  }; 
+
+  ((alloc(args)), ...);
 }
 
 int main() {
+  int v_1 = 3;
+  double v_2 = 3.14;
+  std::string v_3 = "SysPro";
+  Pair v_4(1, 2);
 
-  char memory[sizeof(int) + sizeof(double) + sizeof(std::string)];   // 4 + 8 + 24 = 36 
+  const size_t memory_size = sizeof(v_1) + sizeof(v_2) + sizeof(v_3) + sizeof(v_4);
+  char memory[memory_size]; 
 
-  allocate<sizeof(memory)>(memory, 10, 3.14, std::string("Hello"));
-
+  allocate<memory_size>(memory, v_1, v_2, v_3, v_4);
 
   int* buf_1 = (int*) (memory); 
-  double* buf_2 = (double*) (memory + sizeof(int)); 
-  std::string* buf_3 = (std::string*) (memory + sizeof(int) + sizeof(double)); 
+  double* buf_2 = (double*) (memory + sizeof(v_1)); 
+  std::string* buf_3 = (std::string*) (memory + sizeof(v_1) + sizeof(v_2)); 
+  Pair* buf_4 = (Pair*) (memory + sizeof(v_1) + sizeof(v_2) + sizeof(v_3)); 
 
   std::cout << *buf_1 << std::endl; 
   std::cout << *buf_2 << std::endl; 
   std::cout << *buf_3 << std::endl; 
+  std::cout << buf_4->getFirst() << " " << buf_4->getSecond() << std::endl; 
 
   return 0;
 }
